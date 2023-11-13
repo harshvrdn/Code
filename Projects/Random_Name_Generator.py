ef969f0b4546a02712201dec06ac03643e0eb03f
Harsh Vardhan <harshvrdhndbg1@gmail.com>
2023-11-13 13:11:59 +0530

    wording changes

---
+++ Projects/Random_Name_Generator.py
@@ -0,0 +1,4 @@
+Greetings = print("Hello and Welcome to the Random Name Generator !\n")
+Name = input("What is the city that you were born in ?\n")
+fav_cartoon = input("What is the name of the fictional character you like ?\n")
+rand_name = print("The name should be" + " " + Name + " " + fav_cartoon)
